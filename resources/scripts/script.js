e2237e233d17714ae6e96ee86372ea56c4c99841
Melanie Phillips <melanieleeking@gmail.com>
2018-02-07 12:49:10 -0500

    Initial commit.

---
+++ resources/scripts/script.js
@@ -0,0 +1,14 @@
+const shipHome = {};
+
+shipHome.answers = {
+    nostromo: 0,
+    milano: 0,
+    millfalc: 0, 
+};
+
+// Need to allow user to only select one answer to each question
+// On submit, need to pull all selections
+// Need to evaluate all selections 
+// Need to total points for each ship
+// Need to determine which ship is their answer
+// Need to display ship/answer
