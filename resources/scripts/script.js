bc6890aac1ad7028314ad4e9fd3e24ecbbb376f2
Melanie Phillips <melanieleeking@gmail.com>
2018-02-08 17:31:14 -0500

    Script done for question tallies.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -9,11 +9,84 @@ shipHome.answers = {
     serenity: 0,
     eaglefive: 0,
     rocinante: 0
-};
+}
 
-// Need to allow user to only select one answer to each question
-// On submit, need to pull all selections
-// Need to evaluate all selections 
-// Need to total points for each ship
-// Need to determine which ship is their answer
+shipHome.events = function () {
+    $('form').on('submit', function(event){
+        // This is where question 1 starts
+        const shipValQ1 = $('input[name="q1"]:checked').attr('class');
+        const shipArrayQ1 = shipValQ1.split(" ");
+        for(let key in shipHome.answers) {
+            shipArrayQ1.forEach(function(item){
+                if(item === key) {
+                    shipHome.answers[key] = shipHome.answers[key] + 1
+                }
+            });
+        };
+        // This is whee question 2 starts
+        const shipValQ2 = $('input[name="q2"]:checked').attr('class');
+        // console.log(shipValQ2);
+        const shipArrayQ2 = shipValQ2.split(" ");
+        for (let key in shipHome.answers) {
+            shipArrayQ2.forEach(function (item) {
+                if (item === key) {
+                    shipHome.answers[key] = shipHome.answers[key] + 1
+                }
+            });
+        };
+        // This is whee question 3 starts
+        const shipValQ3 = $('input[name="q3"]:checked').attr('class');
+        // console.log(shipValQ3);
+        const shipArrayQ3 = shipValQ3.split(" ");
+        for (let key in shipHome.answers) {
+            shipArrayQ3.forEach(function (item) {
+                if (item === key) {
+                    shipHome.answers[key] = shipHome.answers[key] + 1
+                }
+            });
+        };
+        // This is where question 4 starts
+        const shipValQ4 = $('input[name="q4"]:checked').attr('class');
+        // console.log(shipValQ4);
+        const shipArrayQ4 = shipValQ4.split(" ");
+        for (let key in shipHome.answers) {
+            shipArrayQ4.forEach(function (item) {
+                if (item === key) {
+                    shipHome.answers[key] = shipHome.answers[key] + 1
+                }
+            });
+        };
+        // This is where question 5 starts
+        const shipValQ5 = $('input[name="q5"]:checked').attr('class');
+        // console.log(shipValQ5);
+        const shipArrayQ5 = shipValQ5.split(" ");
+        for (let key in shipHome.answers) {
+            shipArrayQ5.forEach(function (item) {
+                if (item === key) {
+                    shipHome.answers[key] = shipHome.answers[key] + 1
+                }
+            });
+        };
+        console.log(shipHome.answers);
+        // This is where questions end
+        // This is where answer generation begins
+        return shipHome.answers;
+        const finalAnswer = Math.max(shipHome.answers);
+        // console.log(finalAnswer);
+    });
+}
+
+shipHome.init = function () {
+    shipHome.events();
+}
+
+$(function(){
+    shipHome.init();
+});
+
+// Need to allow user to only select one answer to each question - Done
+// On submit, need add +1 to each property in shipHome where the answer was ticked - Done
+// Need to evaluate totals for each ship
+// Need to determine which ship is the highest number
+// Need to also determine what happens if there is a tie (random?)
 // Need to display ship/answer
